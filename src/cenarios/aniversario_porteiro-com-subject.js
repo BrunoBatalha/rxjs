5af3378f725397aadfc3d0bcef5e4a84083e9cba
Bruno Batalha <brunobatalha37@gmail.com>
2021-10-10 18:37:04 -0400

    adjust example

--- src/cenarios/aniversario_porteiro-com-subject.js
+++ src/cenarios/aniversario_porteiro-com-subject.js
@@ -9,20 +9,30 @@ o aniversariante estiver chegando
 
 const { Subject } = require("rxjs");
 const porteiroSubject = new Subject();
-
-function ouvidoDoLiderDaFesta(value) {
-  console.log("Lider recebeu mensagem: " + value);
+const liderDaFestaObserver = {
+  next(mensagem){
+    console.log("Lider recebeu mensagem: " + mensagem);    
+  },
+  complete(){
+    console.log('Lider desliga as luzes')
+  }
 }
+
+
 
 console.log('Lider pediu favor do porteiro');
 // começar a olhar
-porteiroSubject.subscribe(ouvidoDoLiderDaFesta);
+porteiroSubject.subscribe(liderDaFestaObserver);
 console.log('Lider subiu pro apartamento');
 
 let count = 0;
 const interval = setInterval(() => {
+  if(count == 4){    
+    porteiroSubject.next('Acho que vi o carro... Ah não era não')
+  }
   if (count == 5) {
     porteiroSubject.next("ELE chegou!");
+    porteiroSubject.complete();
     clearInterval(interval);
   }
   count++;
